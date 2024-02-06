06c02c76826841ae4e388299ebd55a9885fd8fe9
Shun Inoue <shun.inoue11@gmail.com>
2024-02-06 20:27:15 +0900

    Add MUI and its settings

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript, DocumentProps, DocumentContext } from 'next/document'
+import {
+  DocumentHeadTags,
+  documentGetInitialProps,
+  DocumentHeadTagsProps,
+} from '@mui/material-nextjs/v13-pagesRouter'
 
-export default function Document() {
+export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <DocumentHeadTags {...props} />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
   )
+}
+
+Document.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx)
+  return finalProps
 }
