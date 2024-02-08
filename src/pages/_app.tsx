5092bd04ed7e05ab1c740266f6f5256b4bd39919
Shun Inoue <shun.inoue11@gmail.com>
2024-02-08 15:09:02 +0900

    init top page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
@@ -22,6 +21,7 @@ const theme = createTheme({
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props
+
   return (
     <AppCacheProvider {...props}>
       <ThemeProvider theme={theme}>
